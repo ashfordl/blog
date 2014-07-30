fe47387aad3dc4aa2ec9ae60305a672e1b4911cd
Luke Ashford <luke_a_ashford@yahoo.co.uk>
2014-07-30 10:37:53 +0100

    Refactor User validation into the model

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -34,24 +34,10 @@ class UserController extends BaseController
      */
     public function postLogin()
     {
-        $data = Input::all();
-        $rules = array(
-            'email'     => 'max:255|email|required',
-            'password'  => 'min:5|required'
-            );
-        $validator = Validator::make($data, $rules);
+        // Retrieve data and attempt login
+        $login = User::attemptLogin(Input::all());
 
-        if($validator->fails())
-        {
-            // Validation fails, do not query database
-            return Redirect::action('UserController@getLogin')
-                        ->with('login_error', true)
-                        ->withInput();
-        }
-        
-        // Validation passes, proceed to query database
-        if (Auth::attempt(array('email' => $data['email'], 'password' => $data['password']), 
-            (isset($data['permanent']) && $data['permanent']) ))
+        if ($login)
         {
             // Login successful
             return Redirect::route('blog');
@@ -82,7 +68,7 @@ class UserController extends BaseController
      * @return View:register
      */
     public function getRegister()
-    {   
+    { 
         return View::make('user.register');
     }
 
@@ -93,34 +79,20 @@ class UserController extends BaseController
      */
     public function postRegister()
     {   
+        // Retrieve data and attempt register
+        $register = User::attemptRegister(Input::all());
 
-        $rules = array(
-            'name'      => 'max:255|required|regex:/^[a-zA-Z0-9- _]*$/',
-            'email'     => 'max:255|email|required|confirmed|unique:users,email',
-            'password'  => 'min:5|required|confirmed'
-        );
-        $validator = Validator::make(Input::all(), $rules);
-
-        if($validator->fails())
+        if ($register)
         {
-            // Validation fails, show errors
-            return Redirect::action('UserController@getRegister')
-                        ->withErrors($validator)
-                        ->withInput();
-        }
-        else 
-        {
-            // Create a new user
-            $user = new User();
-            $user->display_name = Input::get('name');
-            $user->email = Input::get('email');
-            $user->password = Hash::make(Input::get('password'));
-            $user->save();
-
-            // Log him in
-            Auth::login($user);
-
+            // Register successful
             return Redirect::route('blog');
+        }
+        else
+        {
+            // Login unsuccessful
+            return Redirect::action('UserController@getRegister')
+                        ->withErrors(User::$registerValidator)
+                        ->withInput();
         }
     }
 
@@ -150,41 +122,29 @@ class UserController extends BaseController
         $successes = array();
 
         // If the name has been changed
-        if(Input::get('name') != Auth::user()->display_name)
+        if (Input::get('name') != Auth::user()->display_name)
         {
-            $rules = array(
-                'name'  => 'max:255|regex:/^[a-zA-Z0-9- _]*$/'
-            );
-            $validator = Validator::make($data, $rules);
-
-            if($validator->passes())
+            if (User::attemptUpdateName($data))
             {
-                $user = Auth::user();
-                $user->display_name = Input::get('name');
-                $user->save();
                 array_push($successes, 'Display name changed successfully');
             }
-            else   // If validation fails, add the error message to the array
-                array_push($errors, $validator->messages()->first());
+            else
+            {
+                array_push($errors, User::$updateNameValidator->messages()->first());
+            }
         }
 
+        // If the password has been changed
         if(Input::get('cur_password') != '' || Input::get('new_password') != '')
         {
-            $rules = array(
-                'cur_password'  => 'passcheck|required',
-                'new_password'  => 'required|min:5|confirmed'
-            );
-            $validator = Validator::make($data, $rules);
-
-            if($validator->passes())
+            if (User::attemptUpdatePassword($data))
             {
-                $user = Auth::user();
-                $user->password = Hash::make(Input::get('new_password'));
-                $user->save();
                 array_push($successes, 'Password changed successfully');
             }
-            else   // If validation fails, add the error message to the array
-                array_push($errors, $validator->messages()->first());
+            else
+            {
+                array_push($errors, User::$updatePasswordValidator->messages()->first());
+            }
         }
 
         return Redirect::action('UserController@getSettings')
--- app/models/User.php
+++ app/models/User.php
@@ -4,6 +4,193 @@ use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
+    /**
+     * The validation rules to login.
+     *
+     * @var array
+     */
+    protected static $loginRules = array(
+            'email'     => 'max:255|email|required',
+            'password'  => 'min:5|required'
+        );
+
+    /**
+     * The validation rules to register.
+     *
+     * @var array
+     */
+    protected static $regRules = array(
+            'name'      => 'max:255|required|regex:/^[a-zA-Z0-9- _]*$/',
+            'email'     => 'max:255|email|required|confirmed|unique:users,email',
+            'password'  => 'min:5|required|confirmed'
+        );
+
+    /**
+     * The validation rules to update the name.
+     *
+     * @var array
+     */
+    protected static $nameRules = array(
+            'name'  => 'max:255|regex:/^[a-zA-Z0-9- _]*$/'
+        );
+
+    /**
+     * The validation rules to update the password.
+     *
+     * @var array
+     */
+    protected static $passRules = array(
+            'cur_password'  => 'passcheck|required',
+            'new_password'  => 'required|min:5|confirmed'
+        );
+
+    /**
+     * Attempts to login a user with passed data.
+     *
+     * @return bool
+     */
+    public static function attemptLogin($data)
+    {
+        // Validate input
+        $validator = Validator::make($data, User::$loginRules);
+
+        if ($validator->fails())
+        {
+            // Invalid input
+            return false;
+        }
+
+        // Input is valid, attempt auth
+        if (Auth::attempt(array('email' => $data['email'], 'password' => $data['password']), 
+            (isset($data['permanent']) && $data['permanent']) ))
+        {
+            // Successful login
+            return true;
+        }
+        else
+        {
+            // Error
+            return false;
+        }
+    }
+
+    /**
+     * The validator last used to register a user.
+     *
+     * @var Validator
+     */
+    public static $registerValidator;
+
+    /**
+     * Attempts to register a user with passed data.
+     *
+     * @return bool
+     */
+    public static function attemptRegister($data)
+    {
+        // Validate input
+        $validator = Validator::make($data, User::$regRules);
+
+        if ($validator->fails())
+        {
+            // Invalid input
+            User::$registerValidator = $validator;
+            return false;
+        }
+        else
+        {
+            // Create a new user
+            $user = new User();
+            $user->display_name = Input::get('name');
+            $user->email = Input::get('email');
+            $user->password = Hash::make(Input::get('password'));
+            $user->save();
+
+            // Log him in
+            Auth::login($user);
+
+            // Set validator
+            User::$registerValidator = $validator;
+
+            return true;
+        }
+    }
+
+    /**
+     * The validator last used to update the user's name.
+     *
+     * @var Validator
+     */
+    public static $updateNameValidator;
+
+    /**
+     * Attempts to update the user's name.
+     *
+     * @return bool
+     */
+    public static function attemptUpdateName($data)
+    {
+        // Valdiate input
+        $validator = Validator::make($data, User::$nameRules);
+
+        if ($validator->passes())
+        {
+            // Valid input; update the name
+            $user = Auth::user();
+            $user->display_name = $data['name'];
+            $user->save();
+
+            // Set validator
+            User::$updateNameValidator = $validator;
+
+            return true;
+        }
+        else
+        {
+            // Invalid input
+            User::$updateNameValidator = $validator;
+
+            return false;
+        }
+    }
+
+    /**
+     * The validator last used to update the user's password.
+     *
+     * @var Validator
+     */
+    public static $updatePasswordValidator;
+
+    /**
+     * Attempts to update the user's password.
+     *
+     * @return bool
+     */
+    public static function attemptUpdatePassword($data)
+    {
+        // Valdiate input
+        $validator = Validator::make($data, User::$passRules);
+
+        if ($validator->passes())
+        {
+            // Valid input; update the password
+            $user = Auth::user();
+            $user->password = Hash::make(Input::get('new_password'));
+            $user->save();
+
+            // Set validator
+            User::$updatePasswordValidator = $validator;
+
+            return true;
+        }
+        else
+        {
+            // Invalid input
+            User::$updatePasswordValidator = $validator;
+
+            return false;
+        }
+    }
 
 	/**
 	 * The database table used by the model.
@@ -48,5 +235,12 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	{
 		return $this->email;
 	}
+
+    public function isAdmin()
+    {
+        if ($this->id == 1)
+            return true;
+        return false;
+    }
 
 }
