1b18206b10fb3d999a95b7ac9b3a5ec7d08efdc2
Luke Ashford <luke_a_ashford@yahoo.co.uk>
2014-07-31 22:33:52 +0100

    Add ban info table

--- app/views/admin/user/user.blade.php
+++ app/views/admin/user/user.blade.php
@@ -21,4 +21,31 @@
             <td>{{{ $user->created_at }}}</td>
         </tr>
     </table>
+
+    <h3>Bans</h3>
+    @if (count($user->receivedBans()->get()) == 0)
+        <p>This user has received no bans.</p>
+    @else
+        <table>
+            <tr>
+                <th>Issued By</th>
+                <th>Start</th>
+                <th>Finish</th>
+                <th>Validity</th>
+                <th>Type</th>
+                <th>Comment</th>
+            </tr>
+        @foreach ($user->receivedBans()->get() as $ban)
+            <tr>
+                <td>{{{ $ban->issuer->display_name }}}</td>
+                <td>{{{ $ban->start }}}</td>
+                <td>{{{ $ban->end }}}</td>
+                <td>{{{ $ban->valid ? 'Valid' : 'Invalid' }}}</td>
+                <td>{{{ $ban->type->name }}}</td>
+                <td>{{{ $ban->comment }}}</td>
+            </tr>
+        @endforeach
+        </table>
+    @endif
+    
 @stop
