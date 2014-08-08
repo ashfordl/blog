652f89b521b84b85d0e8a8ad7579d621944258b1
Luke Ashford <luke_a_ashford@yahoo.co.uk>
2014-08-08 22:17:18 +0100

    Update admin index pages

--- app/views/admin/blog/index.blade.php
+++ app/views/admin/blog/index.blade.php
@@ -8,16 +8,16 @@
     <h1>Blog Admin</h1>
 
     <a href="{{ action('BlogAdminController@getPost') }}"><h4>Create post</h4></a>
-    <h4>Edit posts</h4>
+    <h4>Posts</h4>
 
     <div class="col-md-8 col-lg-6">
-        <table class="table table-hover col-md-8 col-lg-6">
+        <table class="table table-hover">
             <tr>
                 <th>ID</th>
                 <th>Title <small>Click to edit</small></th>
                 <th>Date</th>
                 <th>Status</th>
-                <th></th>
+                <th>Link</th>
             </tr>
         @foreach($posts as $post)
             <tr>
@@ -25,11 +25,11 @@
                 <td><a href="{{ action('BlogAdminController@getPost', array($post->id)) }}">{{{ $post->title }}}</a></td>
                 <td>{{{ $post->created_at }}}</td>
                 <td>{{{ $post->deleted ? 'Hidden' : 'Visible' }}}</td>
-                @if (!$post->deleted)
-                    <td><a href="{{ action('BlogController@getPost', array($post->id)) }}">View</a></td>
-                @else
-                    <td></td>
-                @endif
+                
+                <td>@if (!$post->deleted)
+                    <a href="{{ action('BlogController@getPost', array($post->id)) }}">View</a>
+                @endif</td>
+                
             </tr>
         @endforeach
         </table>
--- app/views/admin/user/index.blade.php
+++ app/views/admin/user/index.blade.php
@@ -6,23 +6,28 @@
 
 @section('body')
     <h1>User Admin</h1>
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Display Name</th>
-            <th>E-Mail</th>
-            <th>Created At</th>
-        </tr>
-    @foreach($users as $user)
-        <tr>
-            <td>{{{ $user->id }}}</td>
-            <td><a href="{{ action('UserAdminController@getView', $user->id) }}">{{{ $user->display_name }}}</a></td>
-            <td>{{{ $user->email }}}</td>
-            <td>{{{ $user->created_at }}}</td>
-            @if ($user->isBanned())
-                <td><span>BANNED</span></td>
-            @endif
-        </tr>
-    @endforeach
-    </table>
+
+    <h4>Users</h4>
+    <div class="col-md-8 col-lg-6">
+        <table class="table table-hover">
+            <tr>
+                <th>ID</th>
+                <th>Display Name <small>Click to view</small></th>
+                <th>Email</th>
+                <th>Created At</th>
+                <th>Ban Status</th>
+            </tr>
+        @foreach($users as $user)
+            <tr>
+                <td>{{{ $user->id }}}</td>
+                <td><a href="{{ action('UserAdminController@getView', $user->id) }}">{{{ $user->display_name }}}</a></td>
+                <td>{{{ $user->email }}}</td>
+                <td>{{{ $user->created_at }}}</td>
+                <td>@if ($user->isBanned())
+                    Banned
+                @endif </td>
+            </tr>
+        @endforeach
+        </table>
+    </div>
 @stop
