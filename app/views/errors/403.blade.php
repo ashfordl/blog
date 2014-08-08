a4456959d7a4226f188cb4d3f30fd58c27356bad
Luke Ashford <luke_a_ashford@yahoo.co.uk>
2014-08-08 18:19:27 +0100

    Update error views

--- app/views/errors/403.blade.php
+++ app/views/errors/403.blade.php
@@ -6,5 +6,5 @@
 
 @section('body')
     <h1>403 Access Denied</h1>
-    <h3>You do not have permission to access this page. <a href="{{ route('home') }}">Click here to return to home.</a></h3>
+    <h4>You do not have permission to access this page. <a href="{{ route('home') }}">Click here to return to home.</a></h4>
 @stop
--- app/views/errors/404.blade.php
+++ app/views/errors/404.blade.php
@@ -6,5 +6,5 @@
 
 @section('body')
     <h1>404 Page Not Found</h1>
-    <h3>Unfortunately this page couldn't be found. Please check your URL. <a href="{{ route('home') }}">Click here to return to home.</a></h3>
+    <h4>Unfortunately this page couldn't be found. Please check your URL. <a href="{{ route('home') }}">Click here to return to home.</a></h4>
 @stop
