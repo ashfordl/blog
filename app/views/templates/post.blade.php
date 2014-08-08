d308a5c8d4f5a3e03adc89ba61cc74953e9f7ab6
Luke Ashford <luke_a_ashford@yahoo.co.uk>
2014-08-08 17:32:15 +0100

    Add bootstrap classes

--- app/views/templates/post.blade.php
+++ app/views/templates/post.blade.php
@@ -1,11 +1,17 @@
-@if (isset($post))
-    <h1>{{{ $post->title }}}</h1>
+<div class="post">
+    @if (isset($post))
+        <h1>{{{ $post->title }}}</h1>
 
-    {{-- Format with paragraph tags, and escape and html entities (eg angle brackets) --}}
-    <p>{{ str_replace(array("\n","\r\n"), "</p><p>", e($post->content)) }}</p>
+        {{-- Escape html entities, format with paragraph tags --}}
+        {{-- Leading and trailing paragraph tags are required for leading and trailing paragraphs respectively --}}
+        <p>{{ str_replace(array("\n","\r\n"), "</p><p>", e($post->content)) }}</p>
 
-    <p>{{{ $post->tags }}}</p>
-@else
-    <h1>Sorry! That post doesn't exist!</h1>
-    <p>Unfortunately the post you were looking for isn't in the database. Maybe try looking in the <a href="{{ action('BlogController@getList') }}">archive</a> for the post you were seeking.</p>
-@endif
+        <div class="well well-sm clearfix">
+            <span class="pull-left">{{{ $post->tags }}}</span>
+            <span class="pull-right">{{ $post->created_at }}</span>
+        </div>
+    @else
+        <h1>Sorry! That post doesn't exist!</h1>
+        <p>Unfortunately the post you were looking for isn't in the database. Maybe try looking in the <a href="{{ action('BlogController@getList') }}">archive</a> for the post you were seeking.</p>
+    @endif
+</div>
