c1ce2194ace027d62f906cb513e5e078fbf6dc34
Luke Ashford <luke_a_ashford@yahoo.co.uk>
2014-08-09 14:23:43 +0100

    Reduce sub-heading size

--- app/views/user/banned.blade.php
+++ app/views/user/banned.blade.php
@@ -7,7 +7,7 @@
 @section('body')
     <h1>Banned</h1>
     <p>You have been banned. Your ban {{ $ban->isPermanent() ? 'is permanent.' : ' expires on '.$ban->end.'.' }}</p>
-    <h4>Issued By</h4><p>{{ $ban->issuer->display_name }}</p>
-    <h4>Comment</h4><p>{{{ $ban->comment }}}</p>
-    <h4>Appeal</h4><p>To appeal, please email appeal&#64;localhost</p>
+    <h5>Issued By</h5><p>{{ $ban->issuer->display_name }}</p>
+    <h5>Comment</h5><p>{{{ $ban->comment }}}</p>
+    <h5>Appeal</h5><p>To appeal, please email <a href="mailto:appeal@localhost">appeal&#64;localhost</a></p>
 @stop
