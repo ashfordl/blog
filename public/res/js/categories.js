b9dc0a0b501a80ce869c206ad56bbda44dcb6069
ashfordl <luke.a.ashford@gmail.com>
2014-08-17 13:13:59 +0100

    Refactor into functions

--- public/res/js/categories.js
+++ public/res/js/categories.js
@@ -1,3 +1,52 @@
+function removeEditingField(text, parent) {
+    // Remove the form
+    parent.empty();
+
+    // Display the original text
+    parent.text(text);
+
+    // Remove the editing class
+    parent.removeClass('editing');
+}
+
+function postEdit(val, btn, td) {
+    // AJAX data
+    var data = {
+        "_token": csrf,
+        "id": td.siblings(':first').text()
+    }
+
+    // Find the header text of the cell's column
+    // Use this as a key in the JSON data, and set the input val as the value
+    data[btn.closest('table').find('tr > th').eq(td.index()).html().toLowerCase()] = val;
+
+    // Make the AJAX call to the server
+    var ajax = $.post(postUrl, data);
+    // Upon success (200)
+    ajax.done(function() {
+        // Remove the form
+        var par = btn.parent().parent();
+        removeEditingField(val, par);
+
+        // Alert the user
+        alert("Category successfully updated.");
+    });
+    // Upon fail (not 200)
+    ajax.fail(function(jqxhr, txt, err) {
+        // If there is a validation error, display it
+        if (jqxhr.status == 400) {
+            alert(jqxhr.responseText);
+        }
+        // Else it's an application/net error
+        else {
+            alert("An error occured.");
+        }
+
+        // Allow the user to try again
+        btn.siblings('textarea').attr("disabled", null);
+    });
+}
+
 $(document).ready(function() {
     // When a normal field is clicked
     $('tr').on('click', '.editable:not(.editing)', function(e) {
@@ -35,46 +84,8 @@ $(document).ready(function() {
         // Retrieve the value and escape string delimeters
         var val = btn.siblings('textarea').val().replace('"', '\"').replace("'", "\'");
 
-        // AJAX data
-        var data = {
-            "_token": csrf,
-            "id": td.siblings(':first').text()
-        }
-
-        // Find the header text of the cell's column
-        // Use this as a key in the JSON data, and set the input val as the value
-        data[btn.closest('table').find('tr > th').eq(td.index()).html().toLowerCase()] = val;
-
-        // Make the AJAX call to the server
-        var ajax = $.post(postUrl, data);
-        ajax.done(function() {
-            // Remove the form
-            var par = btn.parent().parent();
-            par.empty();
-
-            // Display the updated text
-            par.text(val);
-
-            // Alert the user
-            alert("Category successfully updated.");
-
-            // Remove the editing class
-            par.removeClass('editing');
-
-        });
-        ajax.fail(function(jqxhr, txt, err) {
-            // If there is a validation error, display it
-            if (jqxhr.status == 400) {
-                alert(jqxhr.responseText);
-            }
-            // Else it's an application/net error
-            else {
-                alert("An error occured.");
-            }
-
-            // Allow the user to try again
-            btn.siblings('textarea').attr("disabled", null);
-        });
+        // Perform the AJAX request
+        postEdit(val, btn, td);
     });
 
     // When cancel button pressed
@@ -84,12 +95,6 @@ $(document).ready(function() {
         
         // Remove the form
         var par = $(this).parent().parent();
-        par.empty();
-
-        // Display the original text
-        par.text(val);
-
-        // Remove the editing class
-        par.removeClass('editing');
+        removeEditingField(val, par);
     });
 });
