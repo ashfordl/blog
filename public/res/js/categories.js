adaa6a9124d3a8b2648569a24606559d9f76cf88
ashfordl <luke.a.ashford@gmail.com>
2014-08-17 12:11:27 +0100

    Replace text <input> with <textarea>

--- public/res/js/categories.js
+++ public/res/js/categories.js
@@ -6,17 +6,20 @@ $(document).ready(function() {
         // Add editing class
         elm.addClass('editing');
 
+        // Retrieve the current text
         var val = elm.text();
 
+        // Declare the form
         var form = [
             '<form class="form-inline">'
                 ,'<input type="hidden" class="form-control" value="'+val+'">'
-                ,'<input type="text" class="form-control" value="'+val+'">'
+                ,'<textarea class="form-control">'+val+'</textarea>'
                 ,'<button class="btn btn-primary update-cat" type="button"><span class="glyphicon glyphicon-ok"></span></button>'
                 ,'<button class="btn btn-warning cancel-update-cat" type="button"><span class="glyphicon glyphicon-remove"></span></button>'
             ,'</form>'
         ].join('\n');
 
+        // Display the form
         elm.empty();
         elm.append(form);
     });
@@ -26,11 +29,13 @@ $(document).ready(function() {
         var btn = $(this);
         var td = btn.parent().parent();
 
-        btn.siblings('input[type=text]').attr("disabled", "disabled");
+        // Disable the text area
+        btn.siblings('textarea').attr("disabled", "disabled");
 
-        var val = btn.siblings('input[type=text]').val().replace('"', '\"').replace("'", "\'");
-        
-        // AJAX
+        // Retrieve the value and escape string delimeters
+        var val = btn.siblings('textarea').val().replace('"', '\"').replace("'", "\'");
+
+        // AJAX data
         var data = {
             "_token": csrf,
             "id": td.siblings(':first').text()
@@ -40,6 +45,7 @@ $(document).ready(function() {
         // Use this as a key in the JSON data, and set the input val as the value
         data[btn.closest('table').find('tr > th').eq(td.index()).html().toLowerCase()] = val;
 
+        // Make the AJAX call to the server
         var ajax = $.post(postUrl, data);
         ajax.done(function() {
             var par = btn.parent().parent();
@@ -54,7 +60,7 @@ $(document).ready(function() {
         ajax.fail(function() {
             alert("An error occured.")
 
-            btn.siblings('input[type=text]').attr("disabled", null);
+            btn.siblings('textarea').attr("disabled", null);
         });
     });
 
