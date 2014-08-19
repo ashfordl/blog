b4062f3f4797cf54ecff6fddbd2a88f8d59fea20
ashfordl <luke.a.ashford@gmail.com>
2014-08-19 22:32:38 +0100

    Add down migration

--- app/database/migrations/2014_08_18_211408_update_category_id_to_nullable.php
+++ app/database/migrations/2014_08_18_211408_update_category_id_to_nullable.php
@@ -12,7 +12,7 @@ class UpdateCategoryIdToNullable extends Migration {
 	 */
 	public function up()
 	{
-		DB::query("ALTER TABLE `blogposts` MODIFY `category_id` INT(10) UNSIGNED DEFAULT NULL");
+		DB::statement("ALTER TABLE `blogposts` MODIFY `category_id` INT(10) UNSIGNED DEFAULT NULL");
 	}
 
 	/**
@@ -22,7 +22,24 @@ class UpdateCategoryIdToNullable extends Migration {
 	 */
 	public function down()
 	{
-		//
+        // Ensure that there are no null values
+        $id = Category::first()->id;
+		foreach (Blogpost::whereNull('category_id')->get() as $post) 
+        {
+            $post->category_id = $id;
+        }
+
+        Schema::table('blogposts', function($table) 
+            {
+                // Drop the foreign key constraint
+                DB::statement("ALTER TABLE `blogposts` DROP FOREIGN KEY `blogposts_category_id_foreign`");
+
+                // Set to not null
+                DB::statement("ALTER TABLE `blogposts` MODIFY `category_id` INT(10) UNSIGNED NOT NULL");
+
+                // Re-add the foreign key
+                $table->foreign('category_id')->references('id')->on('categories');
+            });
 	}
 
 }
