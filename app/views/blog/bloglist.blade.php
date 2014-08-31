0174e7d3c6085e2b49ad18fdd92c4f8124ab17fd
ashfordl <luke.a.ashford@gmail.com>
2014-08-31 22:16:38 +0100

    Add row container

--- app/views/blog/bloglist.blade.php
+++ app/views/blog/bloglist.blade.php
@@ -5,22 +5,26 @@
 @stop
 
 @section('body')
-    <div class="post col-md-8 col-lg-6">
-        <h1>Archive</h1>
+    <div class="row">
+        <div class="col-md-8 col-lg-6">
+            <h1>Archive</h1>
 
-        <table class="table">
-            <tr>
-                <th>Title</th>
-                <th>Category</th>
-                <th>Published</th>
-            </tr>
-            @foreach($posts as $post)
+            <table class="table">
                 <tr>
-                    <td><a href="{{ action('BlogController@getPost', array($post->id, $post->getTitleURLString())) }}">{{{ $post->title }}}</a></td>
-                    <td>{{{ is_null($post->getCategory()) ? "No category" : $post->getCategory()->title }}}</td>
-                    <td>{{ $post->created_at }}</td>
+                    <th>Title</th>
+                    <th>Category</th>
+                    <th>Published</th>
                 </tr>
-            @endforeach
-        </table>
+                @foreach($posts as $post)
+                    <tr>
+                        <td>
+                            <a href="{{ action('BlogController@getPost', array($post->id, $post->getTitleURLString())) }}">{{{ $post->title }}}</a>
+                        </td>
+                        <td>{{{ is_null($post->getCategory()) ? "No category" : $post->getCategory()->title }}}</td>
+                        <td>{{ $post->created_at }}</td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
     </div>
 @stop
--- app/views/blog/category.blade.php
+++ app/views/blog/category.blade.php
@@ -5,13 +5,15 @@
 @stop
 
 @section('body')
-    <div class="post col-md-8 col-lg-6">
-        <h1>
-            <span>{{{ $category->title }}}</span>
-            <small class="spaced-left">{{ count($posts) }} {{ count($posts) === 1 ? 'post' : 'posts' }}</small>
-        </h1>
-        <p><strong>{{{ $category->description }}}</strong></p>
+    <div class="row">
+        <div class="post col-md-8 col-lg-6">
+            <h1>
+                <span>{{{ $category->title }}}</span>
+                <small class="spaced-left">{{ count($posts) }} {{ count($posts) === 1 ? 'post' : 'posts' }}</small>
+            </h1>
+            <p><strong>{{{ $category->description }}}</strong></p>
 
-        @include('templates.postlist')
+            @include('templates.postlist')
+        </div>
     </div>
 @stop
